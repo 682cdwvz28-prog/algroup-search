901fee7dc8709f70262a878cf91d35b6311a3efd
Михаил <km@MacBook-Pro-Mihail.local>
2026-01-31 22:51:29 +0300

    Fix SMTP_PORT syntax error

--- send_emails.py
+++ send_emails.py
@@ -4,11 +4,11 @@ from email.mime.text import MIMEText
 from typing import List
 
 
-SMTP_HOST = algroup.gate.sendsay.ru("SMTP_HOST")
-SMTP_PORT = 587("SMTP_PORT", "587"))
-SMTP_USER = algroup.algroup@smtpgate("SMTP_USER")
-SMTP_PASSWORD = imi1Xoopiliyabi("SMTP_PASSWORD")
-SMTP_FROM = info@promage.ru("SMTP_FROM", SMTP_USER)
+SMTP_HOST = os.getenv("SMTP_HOST")
+SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+SMTP_FROM = os.getenv("SMTP_FROM", SMTP_USER)
 
 
 def send_emails_smtp(recipients: List[str], subject: str, body: str):
